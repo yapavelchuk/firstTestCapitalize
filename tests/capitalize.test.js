f3e1771026b6050164517b71bb04402bd6a289c3
Yana Pavelchuk <yapavelchuk@yandex.ru>
2022-12-29 17:05:52 +0500

    add assert and change test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
+
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert(capitalize('') === '');
+assert(capitalize('hello') === 'Hello');
 
 console.log('Все тесты пройдены!');
